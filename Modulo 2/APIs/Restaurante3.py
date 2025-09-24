1ce3682e4bc6fb2669dfaceabb5e9b1c1b66f600
We2474 <Pinguito700@gmail.com>
2025-09-24 09:26:51 -0300

    Update Restaurante3.py

--- Modulo 2/APIs/Restaurante3.py
+++ Modulo 2/APIs/Restaurante3.py
@@ -5,23 +5,28 @@ import requests
 passo1 = requests.get(f'https://68ca9f3f430c4476c34a3b5f.mockapi.io/Restaurante/')
 print(passo1.json())
 
+s = input('Vai ficar, ou vai embora? Por favor, digite (ficar/sair) apenas: ')
+
+if s == 'sair':
+    pedidoId=input('digite o id que sera deletado')
+    passo2 = requests.delete(f'https://68ca9f3f430c4476c34a3b5f.mockapi.io/Restaurante/{pedidoId}')
+    
 
 
-id = input('digite seu id para alterar: ')
-
-if passo1 != ("pedido","bebida"):
-    passo2 = requests.delete(f'https://68ca9f3f430c4476c34a3b5f.mockapi.io/Restaurante/pedido/bebida')
+elif s == 'ficar':
+    pedidoId_0=input('digite o id que sera alterado: ')
+    local = input('Qual mesa vão se sentar? ')
     pedido = input ('Qual será seu pedido? ')
     bebida = input('Bebida pra acompanhamento? ')
-    cliente = {
-    'prato':pedido,
-    'bebida':bebida,
+    pedidoCliente={
+        'prato':pedido,
+        'bebida':bebida,
+        'mesa':local
     }
 
 
-    resposta = requests.put(f'https://68ca9f3f430c4476c34a3b5f.mockapi.io/Restaurante/pedido/bebida/{id}',cliente)
+    passo2_0 = requests.put(f'https://68ca9f3f430c4476c34a3b5f.mockapi.io/Restaurante/{pedidoId_0}',pedidoCliente)
 
-else:
-    print('beleza, sem trocas por hoje. ')
+
 
     
